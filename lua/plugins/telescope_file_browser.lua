d91f49a961e250a2edda13a4b8bc5f42f5577dec
Larkin (Work) <88820972+larkin1@users.noreply.github.com>
2025-12-11 17:32:36 +1100

    updated telescope_file_browser.lua

--- lua/plugins/telescope_file_browser.lua
+++ lua/plugins/telescope_file_browser.lua
@@ -1,4 +1,18 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = function ()
+    require('telescope').setup({
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          use_fd = true,
+          respect_gitignore = vim.fn.executable('fd') == 1,
+          depth = 1,
+          auto_depth = true,
+        },
+      },
+    })
+    require('telescope').load_extension('file_browser')
+  end,
 }
