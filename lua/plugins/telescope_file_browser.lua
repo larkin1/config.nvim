c65ec746d4a8105376f2c4922432d55b6f370fec
Larkin (Work) <larkindunlop2@gmail.com>
2026-01-14 15:34:28 +1100

    added go lsp and added exclusions to telescope file search

--- lua/plugins/telescope_file_browser.lua
+++ lua/plugins/telescope_file_browser.lua
@@ -11,8 +11,50 @@ return {
           depth = 4,
           -- auto_depth = true,
           hidden = {file_browser = true, folder_browser = true},
-        },
+                  },
       },
+      defaults = {
+        file_ignore_patterns = {
+          -- VCS
+          "/%.git/",
+
+          -- Python
+          "/%.venv/",
+          "/%venv/",
+          "/%__pycache__/",
+          "/%.pytest_cache/",
+          "/%.mypy_cache/",
+          "/%.ruff_cache/",
+          "/%.tox/",
+          "/%.coverage$",
+          "/%htmlcov/",
+
+          -- Rust
+          "/%target/",
+
+          -- JS / TS
+          "/%node_modules/",
+          "/%dist/",
+          "/%build/",
+          "/%.next/",
+          "/%.turbo/",
+          "/%.parcel-cache/",
+          "/%coverage/",
+
+          -- Go
+          "/%bin/",
+          "/%out/",
+          "/%.gocache/",
+          "/%cover.out$",
+
+          -- Generic build / cache
+          "/%.cache/",
+          "/%.idea/",
+          "/%.vscode/",
+          "/%.DS_Store",
+          "/%Thumbs.db",
+        },
+      }
     })
     require('telescope').load_extension('file_browser')
   end,
