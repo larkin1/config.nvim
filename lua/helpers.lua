b49782a1c8a5aed2a9a5fcfc007220cfae4912ad
Larkin (Work) <88820972+larkin1@users.noreply.github.com>
2026-03-10 10:51:46 +1100

    added treesitter and some highlighters, changed the home screen and added alpha-nvim

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -13,6 +13,12 @@ function M.run_current_file()
   elseif ft == "python" then
     vim.cmd("w")
     vim.cmd("!python3 %")
+  elseif ft == "sh" or ft == "bash" then
+    vim.cmd("w")
+    vim.cmd("!bash %")
+  elseif ft == "vhs" then
+    vim.cmd("w")
+    vim.cmd("!vhs %")
   end
 end
 
